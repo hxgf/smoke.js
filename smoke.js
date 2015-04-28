25d9f0fc19181f154c6db41372572f6820946add
Dennis Monsewicz <dmonsewicz@myemma.com>
2015-04-28 15:27:54 -0400

    adding in demo; updating for jslint

--- smoke.js
+++ smoke.js
@@ -1,174 +1,172 @@
-/*
-	SMOKE.JS - 0.1.3
-	(c) 2011-2013 Jonathan Youngblood
-	demos / documentation: http://smoke-js.com/ 
+/**
+*	SMOKE.JS - 0.1.3
+*	(c) 2011-2013 Jonathan Youngblood
+*	demos / documentation: http://smoke-js.com/
 */
 
-;(function(window, document) {
+(function(window, document) {
 
-	/*jslint browser: true, onevar: true, undef: true, nomen: false, eqeqeq: true, bitwise: true, regexp: true, newcap: true, immed: true */
-	
+	'use strict';
+
 	var smoke = {
-	  smoketimeout: [],
-	  init: false,
-	  zindex: 1000,
-	  i: 0,
-	
+        smoketimeout: [],
+        init: false,
+        zindex: 1000,
+        i: 0,
+
 		bodyload: function(id) {
 			var ff = document.createElement('div');
-					ff.setAttribute('id','smoke-out-'+id);
-					ff.className = 'smoke-base';
-					ff.style.zIndex = smoke.zindex;
-					smoke.zindex++;
-					document.body.appendChild(ff);
+			ff.setAttribute('id','smoke-out-'+id);
+			ff.className = 'smoke-base';
+			ff.style.zIndex = smoke.zindex;
+			smoke.zindex++;
+			document.body.appendChild(ff);
 		},
-	
+
 		newdialog: function() {
 			var newid = new Date().getTime();
-					newid = Math.random(1,99) + newid;	
-	
-			if (!smoke.init) {		
-		    smoke.listen(window,"load", function() {
-			    smoke.bodyload(newid);
+				newid = Math.random(1,99) + newid;
+
+			if (!smoke.init) {
+		    	smoke.listen(window,"load", function() {
+			    	smoke.bodyload(newid);
 				});
-			}else{
-		    smoke.bodyload(newid);		
+			} else {
+		    	smoke.bodyload(newid);
 			}
-	
-			return newid;
+
+			smoke.newid = newid;
 		},
-	
-		forceload: function() {},
-	
+
 		build: function (e, f) {
 			smoke.i++;
-			
+
 			f.stack = smoke.i;
-	
+
 			e = e.replace(/\n/g,'<br />');
 			e = e.replace(/\r/g,'<br />');
-	
+
 			var prompt = '',
 			    ok = 'OK',
 			    cancel = 'Cancel',
 			    classname = '',
 			    buttons = '',
 			    box;
-	
+
 			if (f.type === 'prompt') {
-				prompt = 
+				prompt =
 					'<div class="dialog-prompt">'+
-						'<input id="dialog-input-'+f.newid+'" type="text" ' + (f.params.value ? 'value="' + f.params.value + '"' : '') + ' />'+
+						'<input id="dialog-input-'+smoke.newid+'" type="text" ' + (f.params.value ? 'value="' + f.params.value + '"' : '') + ' />'+
 					'</div>';
 			}
-	
+
 			if (f.params.ok) {
 				ok = f.params.ok;
 			}
-			
+
 			if (f.params.cancel) {
 				cancel = f.params.cancel;
 			}
-			
+
 			if (f.params.classname) {
 				classname = f.params.classname;
 			}
-	
+
 			if (f.type !== 'signal') {
 				buttons = '<div class="dialog-buttons">';
 				if (f.type === 'alert') {
 					buttons +=
-						'<button id="alert-ok-'+f.newid+'">'+ok+'</button>';
+						'<button id="alert-ok-'+smoke.newid+'">'+ok+'</button>';
 				}
 				 else if (f.type === 'quiz') {
-	
+
 					if (f.params.button_1) {
 						buttons +=
-							'<button class="quiz-button" id="'+f.type+'-ok1-'+f.newid+'">'+f.params.button_1+'</button>';
+							'<button class="quiz-button" id="'+f.type+'-ok1-'+smoke.newid+'">'+f.params.button_1+'</button>';
 					}
-	
+
 					if (f.params.button_2) {
 						buttons +=
-							'<button class="quiz-button" id="'+f.type+'-ok2-'+f.newid+'">'+f.params.button_2+'</button>';
+							'<button class="quiz-button" id="'+f.type+'-ok2-'+smoke.newid+'">'+f.params.button_2+'</button>';
 					}
-	
+
 					if (f.params.button_3) {
 						buttons +=
-							'<button class="quiz-button" id="'+f.type+'-ok3-'+f.newid+'">'+f.params.button_3+'</button>';
+							'<button class="quiz-button" id="'+f.type+'-ok3-'+smoke.newid+'">'+f.params.button_3+'</button>';
 					}
 					if (f.params.button_cancel) {
 						buttons +=
-							'<button id="'+f.type+'-cancel-'+f.newid+'" class="cancel">'+f.params.button_cancel+'</button>';
+							'<button id="'+f.type+'-cancel-'+smoke.newid+'" class="cancel">'+f.params.button_cancel+'</button>';
 					}
-	
+
 				}
-				
+
 				 else if (f.type === 'prompt' || f.type === 'confirm') {
 					if (f.params.reverseButtons) {
 						buttons +=
-							'<button id="'+f.type+'-ok-'+f.newid+'">'+ok+'</button>' +
-							'<button id="'+f.type+'-cancel-'+f.newid+'" class="cancel">'+cancel+'</button>';				
+							'<button id="'+f.type+'-ok-'+smoke.newid+'">'+ok+'</button>' +
+							'<button id="'+f.type+'-cancel-'+smoke.newid+'" class="cancel">'+cancel+'</button>';
 					} else {
 						buttons +=
-							'<button id="'+f.type+'-cancel-'+f.newid+'" class="cancel">'+cancel+'</button>'+
-							'<button id="'+f.type+'-ok-'+f.newid+'">'+ok+'</button>';
+							'<button id="'+f.type+'-cancel-'+smoke.newid+'" class="cancel">'+cancel+'</button>'+
+							'<button id="'+f.type+'-ok-'+smoke.newid+'">'+ok+'</button>';
 					}
 				}
 				buttons += '</div>';
 			}
-	
-	
-			box = 
-				'<div id="smoke-bg-'+f.newid+'" class="smokebg"></div>'+
+
+
+			box =
+				'<div id="smoke-bg-'+smoke.newid+'" class="smokebg"></div>'+
 				'<div class="dialog smoke '+classname+'">'+
 					'<div class="dialog-inner">'+
 							e+
 							prompt+
-							buttons+			
+							buttons+
 					'</div>'+
 				'</div>';
-	
-			if (!smoke.init) {		
+
+			if (!smoke.init) {
 				smoke.listen(window,"load", function() {
 					smoke.finishbuild(e,f,box);
 				});
-			} else{
+			} else {
 				smoke.finishbuild(e,f,box);
 			}
-	
+
 		},
-	
+
 		finishbuild: function(e, f, box) {
-		
-			var ff = document.getElementById('smoke-out-'+f.newid);
-	
+
+			var ff = document.getElementById('smoke-out-'+smoke.newid);
+
 			ff.className = 'smoke-base smoke-visible  smoke-' + f.type;
 			ff.innerHTML = box;
-					
+
 			while (ff.innerHTML === "") {
 				ff.innerHTML = box;
 			}
-			
-			if (smoke.smoketimeout[f.newid]) {
-				clearTimeout(smoke.smoketimeout[f.newid]);
+
+			if (smoke.smoketimeout[smoke.newid]) {
+				clearTimeout(smoke.smoketimeout[smoke.newid]);
 			}
-	
+
 			smoke.listen(
-				document.getElementById('smoke-bg-'+f.newid),
-				"click", 
+				document.getElementById('smoke-bg-'+smoke.newid),
+				"click",
 				function () {
-					smoke.destroy(f.type, f.newid);
+					smoke.destroy(f.type);
 					if (f.type === 'prompt' || f.type === 'confirm' || f.type === 'quiz') {
 						f.callback(false);
 					} else if (f.type === 'alert' && typeof f.callback !== 'undefined') {
 						f.callback();
-					}	
+					}
 				}
 			);
-		
-		
+
+
 			switch (f.type) {
-				case 'alert': 
+				case 'alert':
 					smoke.finishbuildAlert(e, f, box);
 					break;
 				case 'confirm':
@@ -187,332 +185,341 @@
 					throw "Unknown type: " + f.type;
 			}
 		},
-		
-		finishbuildAlert: function (e, f, box) {
+
+		finishbuildAlert: function (e, f) {
 			smoke.listen(
-				document.getElementById('alert-ok-'+f.newid),
-				"click", 
+				document.getElementById('alert-ok-'+smoke.newid),
+				"click",
 				function () {
-					smoke.destroy(f.type, f.newid);
+					smoke.destroy(f.type);
 					if (typeof f.callback !== 'undefined') {
 						f.callback();
 					}
 				}
 			);
-		
+
 			document.onkeyup = function (e) {
 				if (!e) {
 					e = window.event;
 				}
 				if (e.keyCode === 13 || e.keyCode === 32 || e.keyCode === 27) {
-					smoke.destroy(f.type, f.newid);
+					smoke.destroy(f.type);
 					if (typeof f.callback !== 'undefined') {
 						f.callback();
-					}					
+					}
 				}
-			};	
+			};
 		},
-		
-		finishbuildConfirm: function (e, f, box) {
+
+		finishbuildConfirm: function (e, f) {
 			smoke.listen(
-				document.getElementById('confirm-cancel-' + f.newid),
-				"click", 
-				function () 
-				{
-					smoke.destroy(f.type, f.newid);
+				document.getElementById('confirm-cancel-' + smoke.newid),
+				"click",
+				function () {
+					smoke.destroy(f.type);
 					f.callback(false);
 				}
 			);
-			
+
 			smoke.listen(
-				document.getElementById('confirm-ok-' + f.newid),
-				"click", 
-				function () 
-				{
-					smoke.destroy(f.type, f.newid);
+				document.getElementById('confirm-ok-' + smoke.newid),
+				"click",
+				function () {
+					smoke.destroy(f.type);
 					f.callback(true);
 				}
 			);
-					
+
 			document.onkeyup = function (e) {
 				if (!e) {
 					e = window.event;
 				}
 				if (e.keyCode === 13 || e.keyCode === 32) {
-					smoke.destroy(f.type, f.newid);
+					smoke.destroy(f.type);
 					f.callback(true);
 				} else if (e.keyCode === 27) {
-					smoke.destroy(f.type, f.newid);
-					f.callback(false);
-				}
-			};	
-		},
-		
-		finishbuildQuiz: function (e, f, box) {
-			var a, b, c;
-			
-			smoke.listen(
-				document.getElementById('quiz-cancel-' + f.newid),
-				"click", 
-				function () 
-				{
-					smoke.destroy(f.type, f.newid);
-					f.callback(false);
-				}
-			);
-	
-	
-			if (a = document.getElementById('quiz-ok1-'+f.newid))
-			smoke.listen(
-				a,
-				"click", 
-				function () {
-					smoke.destroy(f.type, f.newid);
-					f.callback(a.innerHTML);
-				}
-			);
-	
-	
-			if (b = document.getElementById('quiz-ok2-'+f.newid))
-			smoke.listen(
-				b,
-				"click", 
-				function () {
-					smoke.destroy(f.type, f.newid);
-					f.callback(b.innerHTML);
-				}
-			);
-	
-	
-			if (c = document.getElementById('quiz-ok3-'+f.newid))
-			smoke.listen(
-				c,
-				"click", 
-				function () {
-					smoke.destroy(f.type, f.newid);
-					f.callback(c.innerHTML);
-				}
-			);
-	
-			document.onkeyup = function (e) {
-				if (!e) {
-					e = window.event;
-				}
-				if (e.keyCode === 27) {
-					smoke.destroy(f.type, f.newid);
-					f.callback(false);
-				}
-			};	
-		
-		},
-		
-		finishbuildPrompt: function (e, f, box) {
-			var pi = document.getElementById('dialog-input-'+f.newid);
-				
-			setTimeout(function () {
-				pi.focus();
-				pi.select();
-			}, 100);
-		
-			smoke.listen(
-				document.getElementById('prompt-cancel-'+f.newid),
-				"click", 
-				function () {
-					smoke.destroy(f.type, f.newid);
-					f.callback(false);
-				}
-			);
-		
-			smoke.listen(
-				document.getElementById('prompt-ok-'+f.newid),
-				"click", 
-				function () {
-					smoke.destroy(f.type, f.newid);
-					f.callback(pi.value);
-				}
-			);
-					
-			document.onkeyup = function (e) {
-				if (!e) {
-					e = window.event;
-				}
-				
-				if (e.keyCode === 13) {
-					smoke.destroy(f.type, f.newid);
-					f.callback(pi.value);
-				} else if (e.keyCode === 27) {
-					smoke.destroy(f.type, f.newid);
+					smoke.destroy(f.type);
 					f.callback(false);
 				}
 			};
 		},
-		
-		finishbuildSignal: function (e, f, box) {
-	
-	
+
+		finishbuildQuiz: function (e, f) {
+			var a = document.getElementById('quiz-ok1-'+smoke.newid),
+				b = document.getElementById('quiz-ok2-'+smoke.newid),
+				c = document.getElementById('quiz-ok3-'+smoke.newid);
+
+			smoke.listen(
+				document.getElementById('quiz-cancel-' + smoke.newid),
+				"click",
+				function () {
+					smoke.destroy(f.type);
+					f.callback(false);
+				}
+			);
+
+
+			if (a) {
+				smoke.listen(
+					a,
+					"click",
+					function () {
+						smoke.destroy(f.type);
+						f.callback(a.innerHTML);
+					}
+				);
+			}
+
+
+			if (b) {
+				smoke.listen(
+					b,
+					"click",
+					function () {
+						smoke.destroy(f.type);
+						f.callback(b.innerHTML);
+					}
+				);
+			}
+
+
+			if (c) {
+				smoke.listen(
+					c,
+					"click",
+					function () {
+						smoke.destroy(f.type);
+						f.callback(c.innerHTML);
+					}
+				);
+			}
+
 			document.onkeyup = function (e) {
 				if (!e) {
 					e = window.event;
 				}
 				if (e.keyCode === 27) {
-					smoke.destroy(f.type, f.newid);
+					smoke.destroy(f.type, smoke.newid);
+					f.callback(false);
+				}
+			};
+
+		},
+
+		finishbuildPrompt: function (e, f) {
+			var pi = document.getElementById('dialog-input-'+smoke.newid);
+
+			setTimeout(function () {
+				pi.focus();
+				pi.select();
+			}, 100);
+
+			smoke.listen(
+				document.getElementById('prompt-cancel-'+smoke.newid),
+				"click",
+				function () {
+					smoke.destroy(f.type);
+					f.callback(false);
+				}
+			);
+
+			smoke.listen(
+				document.getElementById('prompt-ok-'+smoke.newid),
+				"click",
+				function () {
+					smoke.destroy(f.type);
+					f.callback(pi.value);
+				}
+			);
+
+			document.onkeyup = function (e) {
+				if (!e) {
+					e = window.event;
+				}
+
+				if (e.keyCode === 13) {
+					smoke.destroy(f.type);
+					f.callback(pi.value);
+				} else if (e.keyCode === 27) {
+					smoke.destroy(f.type);
+					f.callback(false);
+				}
+			};
+		},
+
+		finishbuildSignal: function (e, f) {
+
+			document.onkeyup = function (e) {
+				if (!e) {
+					e = window.event;
+				}
+
+				if (e.keyCode === 27) {
+					smoke.destroy(f.type);
+
 					if (typeof f.callback !== 'undefined') {
 						f.callback();
 					}
 				}
-			};	
-	
-			smoke.smoketimeout[f.newid] = setTimeout(function () {
-				smoke.destroy(f.type, f.newid);
+			};
+
+			smoke.smoketimeout[smoke.newid] = setTimeout(function () {
+				smoke.destroy(f.type);
 				if (typeof f.callback !== 'undefined') {
 					f.callback();
 				}
 			}, f.timeout);
 		},
-		
-				
-		destroy: function (type,id) {
-	
-			var box = document.getElementById('smoke-out-'+id);
-	
+
+
+		destroy: function (type) {
+
+			var id = smoke.newid,
+                box = document.getElementById('smoke-out-'+id),
+				okButton;
+
 			if (type !== 'quiz') {
-			    var okButton = document.getElementById(type+'-ok-'+id);
+			    okButton = document.getElementById(type+'-ok-'+id);
 			}
-	
-	    var cancelButton = document.getElementById(type+'-cancel-'+id);
+
+	    	var cancelButton = document.getElementById(type+'-cancel-'+id);
 			box.className = 'smoke-base';
-	
+
 			if (okButton) {
 				smoke.stoplistening(okButton, "click", function() {});
 				document.onkeyup = null;
 			}
-			
+
 			if (type === 'quiz') {
 				var quiz_buttons = document.getElementsByClassName("quiz-button");
 				for (var i = 0; i < quiz_buttons.length; i++) {
-				smoke.stoplistening(quiz_buttons[i], "click", function() {});
-				document.onkeyup = null;
-				}			
+					smoke.stoplistening(quiz_buttons[i], "click", function() {});
+					document.onkeyup = null;
+				}
 			}
-			
+
 			if (cancelButton) {
 				smoke.stoplistening(cancelButton, "click", function() {});
 			}
-			
+
+			// stops processing of destroying a modal twice with timeout
+			if (type === 'signal') {
+				window.clearTimeout(smoke.smoketimeout[smoke.newid]);
+			}
+
 			smoke.i = 0;
 			document.body.removeChild(box);
+			smoke.newid = null;
 		},
-	
+
 		alert: function (e, f, g) {
 			if (typeof g !== 'object') {
 				g = false;
 			}
-			
-			var id = smoke.newdialog();
-			
+
+			smoke.newdialog();
+
 			smoke.build(e, {
 				type:     'alert',
 				callback: f,
-				params:   g,
-				newid:    id
+				params:   g
 			});
 		},
-		
+
 		signal: function (e, f, g) {
 			if (typeof g !== 'object') {
 				g = false;
-			}		
+			}
 
 			var duration = 5000;
 			if (g.duration !== 'undefined'){
 				duration = g.duration;
 			}
-			
-			var id = smoke.newdialog();
+
+			smoke.newdialog();
 			smoke.build(e, {
 				type:    'signal',
 				callback: f,
 				timeout: duration,
-				params:  g,
-				newid:   id
+				params:  g
 			});
 		},
-		
+
 		confirm: function (e, f, g) {
 			if (typeof g !== 'object') {
 				g = false;
 			}
-			
-			var id = smoke.newdialog();
+
+			smoke.newdialog();
 			smoke.build(e, {
 				type:     'confirm',
 				callback: f,
-				params:   g,
-				newid:    id
+				params:   g
 			});
 		},
-		
+
 		quiz: function (e, f, g) {
 			if (typeof g !== 'object') {
 				g = false;
 			}
-			
-			var id = smoke.newdialog();
+
+			smoke.newdialog();
 			smoke.build(e, {
 				type:     'quiz',
 				callback: f,
-				params:   g,
-				newid:    id
+				params:   g
 			});
 		},
-		
+
 		prompt: function (e, f, g) {
 			if (typeof g !== 'object') {
 				g = false;
 			}
-			
-			var id = smoke.newdialog();
-			return smoke.build(e,{type:'prompt',callback:f,params:g,newid:id});
+
+			smoke.newdialog();
+			return smoke.build(e, {
+                type:'prompt',
+                callback:f,
+                params:g
+            });
 		},
-		
+
 		listen: function (e, f, g) {
-	    if (e.addEventListener) {
-	      return e.addEventListener(f, g, false);
-	    } 
-	    
-	    if (e.attachEvent) {
-	      return e.attachEvent('on'+f, g);
-	    } 
-	    
+    	    if (e.addEventListener) {
+                return e.addEventListener(f, g, false);
+    	    }
+
+    	    if (e.attachEvent) {
+                return e.attachEvent('on'+f, g);
+    	    }
+
 			return false;
 		},
-		
-		stoplistening: function (e, f, g) {	
-	    if (e.removeEventListener) {
-	      return e.removeEventListener(f, g, false);
-	    }
-	    
-	    if (e.detachEvent) {
-	      return e.detachEvent('on'+f, g);
-	    }
-	    
-	    return false;
+
+		stoplistening: function (e, f, g) {
+		    if (e.removeEventListener) {
+		      return e.removeEventListener(f, g, false);
+		    }
+
+		    if (e.detachEvent) {
+		      return e.detachEvent('on'+f, g);
+		    }
+
+		    return false;
 		}
 	};
-	
-	
+
+
 	smoke.init = true;
 
-	if (typeof module != 'undefined' && module.exports) {
+	if (typeof module !== 'undefined' && module.exports) {
 		module.exports = smoke;
-	}
-	else if (typeof define === 'function' && define.amd) {
+	} else if (typeof define === 'function' && define.amd) {
 		define('smoke', [], function() {
 		    return smoke;
 		});
-	}
-	else {
-		this.smoke = smoke;
+	} else {
+		window.smoke = smoke;
 	}
 
 })(window, document);
